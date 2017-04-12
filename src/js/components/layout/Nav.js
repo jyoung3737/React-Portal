b24826fe799ca290074549830fdfbe4f3993940c
Joel Young <joel.young3737@gmail.com>
2017-04-12 12:49:30 -0400

    New Banner and updated team member section

--- src/js/components/layout/Nav.js
+++ src/js/components/layout/Nav.js
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
         <header>
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in">Welcome To Our Studio!</div>
+                <div class="intro-lead-in">Welcome to my online resume!</div>
                 <div class="intro-heading">It's Nice To Meet You</div>
                 <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
             </div>
--- src/js/components/layout/OurTeam.js
+++ src/js/components/layout/OurTeam.js
@@ -9,14 +9,13 @@ export default class OurTeam extends React.Component {
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Our Amazing Team</h2>
+                    <h2 class="section-heading">My Amazing Team</h2>
                     <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                 </div>
             </div>
             <div class="row">
-                <TeamMember />
-                <TeamMember />
-                <TeamMember />
+                <TeamMember name={"Joel Young"} title={"Web Developer & Certified Scrum Master"} />
+                <TeamMember name={"Ollie"} title={"Free-Loader"} />
             </div>
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 text-center">
